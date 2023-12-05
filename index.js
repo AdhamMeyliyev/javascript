7974e305863d46fcdd4c7d1ea243ad65dd4a0fdd
AdhamMeyliyev <adhammeyliyev8@gmail.com>
2023-12-05 14:50:06 +0500

    ......

---
+++ index.js
@@ -0,0 +1,14 @@
+/* console.log("Hello");
+alert("salom"); */
+/* prompt("salomlar"); */
+
+// let num=10;
+
+// console.log(num);
+
+
+// console.log(sum);
+
+// let sum=11;
+
+prompt("Who are you ? , man")
